2963fd7cae91f587f9093de2d77af51faee1f2eb
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-03 15:11:45 +0330

    wrap theme provider and apollo provider to our main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./global.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "@mui/material";
 
+import App from "./App.jsx";
+import theme from "./mui/theme.js";
+import "./styles/global.css";
+import "./styles/fonts.css";
+
+const client = new ApolloClient({
+  uri: "https://api-us-west-2.hygraph.com/v2/cluiex6u002ji07uv6ki4r54r/master",
+  cache: new InMemoryCache(),
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>
 );
