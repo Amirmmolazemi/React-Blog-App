f5d8dd88f341e7e52c204c1d4d87507b00c75998
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-05 15:44:41 +0330

    wrap routers to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/home/HomePage.jsx";
 import Layout from "./components/Layout/Layout.jsx";
+import BlogPage from "./components/blog/BlogPage.jsx";
+import AuthorPage from "./components/author/AuthorPage.jsx";
+import BlogsOnlyPage from "./components/blog/BlogsOnlyPage.jsx";
+import AuthorOnlyPage from "./components/author/AuthorOnlyPage.jsx";
 
 function App() {
   return (
-    <>
-      <Layout>
-        <HomePage />
-      </Layout>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/authors" element={<AuthorOnlyPage />} />
+        <Route path="/blogs" element={<BlogsOnlyPage />} />
+        <Route path="/authors/:slug" element={<AuthorPage />} />
+        <Route path="/blogs/:slug" element={<BlogPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
