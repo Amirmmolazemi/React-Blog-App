57cc4acc29714beb7c5adb6105817ccdcafe59eb
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-05 15:45:07 +0330

    fix:fix the .env bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import theme from "./mui/theme.js";
@@ -9,13 +10,15 @@ import "./styles/global.css";
 import "./styles/fonts.css";
 
 const client = new ApolloClient({
-  uri: import.meta.env.REACT_APP_GRAPHCMS_URI,
+  uri: "https://api-us-west-2.hygraph.com/v2/cluiex6u002ji07uv6ki4r54r/master",
   cache: new InMemoryCache(),
 });
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
+  </BrowserRouter>
 );
