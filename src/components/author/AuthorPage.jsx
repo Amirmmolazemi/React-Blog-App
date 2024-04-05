2a6585d1358c94d32ad5796368ba1c9c3c949a98
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-05 17:07:20 +0330

    feat:add a feature that shows the posts that author posted

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -1,18 +1,22 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import sanitizeHtml from "sanitize-html";
-import { GET_AUTHOR_INFO } from "../../graphql/queries";
+import { GET_AUTHOR_INFO } from "../../graphql/queries.js";
 import { Container, Grid, Avatar, Typography } from "@mui/material";
+import CardEL from "../shared/CardEL.jsx";
+import Loader from "../shared/Loader.jsx";
 
 function AuthorPage() {
   const { slug } = useParams();
   const { loading, data, errors } = useQuery(GET_AUTHOR_INFO, {
     variables: { slug },
   });
-  if (loading) return <h4>loading ....</h4>;
+  if (loading) return <Loader />;
   if (errors) return <h4>error ....</h4>;
 
-  const { author } = data;
+  const {
+    author: { avatar, name, field, description, posts },
+  } = data;
   return (
     <Container maxWidth="lg">
       <Grid container mt={10}>
@@ -23,20 +27,36 @@ function AuthorPage() {
           flexDirection="column"
           alignItems="center"
         >
-          <Avatar src={author.avatar.url} sx={{ width: 250, height: 250 }} />
+          <Avatar src={avatar.url} sx={{ width: 250, height: 250 }} />
           <Typography component="h3" variant="h5" fontWeight={700} mt={4}>
-            {author.name}
+            {name}
           </Typography>
           <Typography component="p" variant="h5" color="text.secondary" mt={2}>
-            {author.field}
+            {field}
           </Typography>
         </Grid>
         <Grid item xs={12}>
           <div
             dangerouslySetInnerHTML={{
-              __html: sanitizeHtml(author.description.html),
+              __html: sanitizeHtml(description.html),
             }}
           ></div>
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <Typography component="h3" variant="h5" fontWeight={700}>
+            مقالات {name}
+          </Typography>
+        </Grid>
+        <Grid container spacing={2} mt={2}>
+          {posts.map((post) => (
+            <Grid item xs={12} sm={6} md={4} key={post.id}>
+              <CardEL
+                title={post.title}
+                slug={post.slug}
+                coverPhoto={post.coverPhoto}
+              />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Container>
