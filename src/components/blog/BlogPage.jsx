147bd06261d4def703be88cc540590a6dc4464ce
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-04-05 17:09:30 +0330

    feat:add a page that shows each blogs details

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,7 +1,75 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_POST_INFO } from "../../graphql/queries.js";
+import Loader from "../shared/Loader.jsx";
+import { Container } from "@mui/system";
+import { Grid, Typography, Avatar, Box } from "@mui/material";
+import { ArrowBackRounded } from "@mui/icons-material";
+import sanitizeHtml from "sanitize-html";
+import CommentForm from "../comments/CommentForm.jsx";
+
 function BlogPage() {
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const { loading, data, errors } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+  if (loading) return <Loader />;
+  if (errors) return <h4>error ....</h4>;
+
+  const {
+    post: { title, coverPhoto, author, content },
+    post,
+  } = data;
+
   return (
-    <div>BlogPage</div>
-  )
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12} mt={9} display="flex" justifyContent="space-between">
+          <Typography
+            component="h2"
+            variant="h4"
+            color="primary"
+            fontWeight={700}
+          >
+            {title}
+          </Typography>
+          <ArrowBackRounded onClick={() => navigate(-1)} />
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <img
+            src={coverPhoto.url}
+            alt={post.slug}
+            width="100%"
+            style={{ borderRadius: 15 }}
+          />
+        </Grid>
+        <Grid item xs={12} mt={7} display="flex" alignItems="center">
+          <Avatar
+            src={author.avatar.url}
+            sx={{ width: 80, height: 80, marginLeft: 2 }}
+          />
+          <Box component="div">
+            <Typography component="p" variant="h5" fontWeight={700}>
+              {author.name}
+            </Typography>
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.field}
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} mt={5}>
+          <div
+            dangerouslySetInnerHTML={{ __html: sanitizeHtml(content.html) }}
+          ></div>
+        </Grid>
+        <Grid item xs={12}>
+          <CommentForm slug={slug} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default BlogPage
+export default BlogPage;
